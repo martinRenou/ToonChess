93c24cf15d0af94d2520f787ca22fa60dc2e8512
martinRenou <martin.renou@gmail.com>
2017-10-12 15:09:59 +0200

    Add window creation with SFML

--- src/3DToonChess.cxx
+++ src/3DToonChess.cxx
@@ -1,5 +1,19 @@
+#include <iostream>
+#include <SFML/Graphics.hpp>
+using namespace std;
 
-int main (int argc, char *argv[])
-{
+int main(int argc, char* argv[]) {
+  sf::Window window(sf::VideoMode(800, 600), "3DToonChess");
+
+  while(window.isOpen()){
+    sf::Event event;
+
+    while(window.pollEvent(event)){
+      if(event.type == sf::Event::Closed) window.close();
+    }
+
+    window.display();
+  }
+
   return 0;
 }
