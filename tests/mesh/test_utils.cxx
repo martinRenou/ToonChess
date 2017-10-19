a94c22059b92230e018420eb2374470e4c172e8f
martinRenou <martin.renou@gmail.com>
2017-10-19 16:03:40 +0200

    Add test for the split function

--- tests/mesh/test_utils.cxx
+++ tests/mesh/test_utils.cxx
@@ -5,11 +5,20 @@
 
 #include "../../src/mesh/utils.hxx"
 
-TEST(SplitTest, CanSplit) {
+TEST(SplitFunctionTest1, CanSplitWithSpaces) {
   std::vector<std::string> out = split("v 1.0 2.5 3.2", ' ');
 
   EXPECT_EQ("v", out[0]);
   EXPECT_EQ("1.0", out[1]);
   EXPECT_EQ("2.5", out[2]);
   EXPECT_EQ("3.2", out[3]);
+}
+
+TEST(SplitFunctionTest2, CanSplitWithDots) {
+  std::vector<std::string> out = split("v 1.0 2.5 3.2", '.');
+
+  EXPECT_EQ("v 1", out[0]);
+  EXPECT_EQ("0 2", out[1]);
+  EXPECT_EQ("5 3", out[2]);
+  EXPECT_EQ("2", out[3]);
 }
