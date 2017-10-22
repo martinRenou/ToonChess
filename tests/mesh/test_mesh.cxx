290939f3663c0fd4d4a98d94a0a2681474a6b5f0
martinRenou <martin.renou@gmail.com>
2017-10-22 17:04:26 +0200

    Add test for the mesh indices

--- tests/mesh/test_mesh.cxx
+++ tests/mesh/test_mesh.cxx
@@ -25,3 +25,21 @@ TEST(mesh, vertices) {
   EXPECT_EQ(1.0, test->vertices.at(22));
   EXPECT_EQ(1.0, test->vertices.at(23));
 }
+
+TEST(mesh, indices) {
+  Mesh* test = new Mesh("../tests/testFixtures/test.obj");
+
+  test->initBuffers();
+
+  EXPECT_EQ(0, test->indices.at(0));
+  EXPECT_EQ(6, test->indices.at(1));
+  EXPECT_EQ(4, test->indices.at(2));
+
+  EXPECT_EQ(0, test->indices.at(3));
+  EXPECT_EQ(2, test->indices.at(4));
+  EXPECT_EQ(6, test->indices.at(5));
+
+  EXPECT_EQ(1, test->indices.at(33));
+  EXPECT_EQ(7, test->indices.at(34));
+  EXPECT_EQ(3, test->indices.at(35));
+}
