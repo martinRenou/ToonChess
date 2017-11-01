9aa2b9e2cbf583753df79b9a79006bbf1b107766
martinRenou <martin.renou@gmail.com>
2017-11-01 13:00:10 +0100

    Fix black border width

--- shaders/blackBorderVertexShader.glsl
+++ shaders/blackBorderVertexShader.glsl
@@ -1,5 +1,7 @@
 void main(void){
-  vec4 deformedPosition = vec4(1.03, 1.03, 1.01, 1.0) * gl_Vertex;
+  float factor = 0.1;
+  vec4 deformation = vec4(factor, factor, factor, 0.0) * normalize(gl_Vertex);
+  vec4 deformedPosition = deformation + gl_Vertex;
 
   // The position of the vertex
   gl_Position = gl_ProjectionMatrix * gl_ModelViewMatrix * deformedPosition;
