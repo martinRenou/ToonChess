1ee6158673e65d6b46e650e9c11d84e40ccb1e58
martinRenou <martin.renou@gmail.com>
2017-12-04 15:33:30 +0100

    Specify clear color in the ColorPicking rendering

--- src/ColorPicking/ColorPicking.cxx
+++ src/ColorPicking/ColorPicking.cxx
@@ -135,6 +135,7 @@ sf::Vector2i ColorPicking::getClickedPiecePosition(
   glBindFramebuffer(GL_FRAMEBUFFER, this->fboId);
 
   // Clear buffers and render
+  glClearColor(1, 1, 1, 1);
   glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
   colorPickingRender(board, meshes, programs);
 
