9d4af9ab8dd1053bcf33e458de61b72d61543b94
martinRenou <martin.renou@gmail.com>
2018-01-05 12:08:06 +0100

    Adapt test folder

--- tests/utils/test_math.cxx
+++ tests/utils/test_math.cxx
@@ -1,35 +1,10 @@
 #include <gtest/gtest.h>
 
+#include <vector>
+
 #include <GL/gl.h>
 
-#include <vector>
-#include <string>
-
-#include "../src/utils.hxx"
-
-TEST(load_file, txt) {
-  std::string out = loadFile("../tests/testFixtures/test.txt");
-
-  EXPECT_EQ("Hello\nWorld !\n!\n", out);
-}
-
-TEST(split, with_spaces) {
-  std::vector<std::string> out = split("v 1.0 2.5 3.2", ' ');
-
-  EXPECT_EQ("v", out[0]);
-  EXPECT_EQ("1.0", out[1]);
-  EXPECT_EQ("2.5", out[2]);
-  EXPECT_EQ("3.2", out[3]);
-}
-
-TEST(split, with_dots) {
-  std::vector<std::string> out = split("v 1.0 2.5 3.2", '.');
-
-  EXPECT_EQ("v 1", out[0]);
-  EXPECT_EQ("0 2", out[1]);
-  EXPECT_EQ("5 3", out[2]);
-  EXPECT_EQ("2", out[3]);
-}
+#include "../../src/utils/math.hxx"
 
 TEST(matrixProduct, with_identity) {
   std::vector<GLfloat> in1 = getIdentityMatrix();
