8f61446e11457b81ffbac1cc808e10eebb8abb1b
martinRenou <martin.renou@gmail.com>
2018-02-25 17:58:57 +0100

    Load piece parts

--- src/mesh/meshes.cxx
+++ src/mesh/meshes.cxx
@@ -1,4 +1,5 @@
 #include <map>
+#include <vector>
 
 #include "loadObjFile.hxx"
 #include "Mesh.hxx"
@@ -9,16 +10,22 @@
 std::map<int, Mesh*> initMeshes(){
   // Create and load meshes
   Mesh* king = loadObjFile("../assets/king.obj").at(0);
+  std::vector<Mesh*> king_parts = loadObjFile("../assets/king_parts.obj");
 
   Mesh* queen = loadObjFile("../assets/queen.obj").at(0);
+  std::vector<Mesh*> queen_parts = loadObjFile("../assets/queen_parts.obj");
 
   Mesh* bishop = loadObjFile("../assets/bishop.obj").at(0);
+  std::vector<Mesh*> bishop_parts = loadObjFile("../assets/bishop_parts.obj");
 
   Mesh* rook = loadObjFile("../assets/rook.obj").at(0);
+  std::vector<Mesh*> rook_parts = loadObjFile("../assets/rook_parts.obj");
 
   Mesh* knight = loadObjFile("../assets/knight.obj").at(0);
+  std::vector<Mesh*> knight_parts = loadObjFile("../assets/knight_parts.obj");
 
   Mesh* pawn = loadObjFile("../assets/pawn.obj").at(0);
+  std::vector<Mesh*> pawn_parts = loadObjFile("../assets/pawn_parts.obj");
 
   Mesh* boardCell = loadObjFile("../assets/boardCell.obj").at(0);
 
