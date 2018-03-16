aa194a8d640e6cce7defbd512664fd5c3fea20d2
martinRenou <martin.renou@gmail.com>
2018-03-16 12:48:07 +0100

    Implement smoke particles generation and update

--- src/SmokeGenerator/SmokeGenerator.cxx
+++ src/SmokeGenerator/SmokeGenerator.cxx
@@ -1,21 +1,36 @@
 #define GL_GLEXT_PROTOTYPES
 
+#include <vector>
+#include <algorithm>
+#include <iostream>
+
 #include <GL/gl.h>
 
 #include <SFML/Graphics.hpp>
 
 #include "SmokeGenerator.hxx"
 
-SmokeGenerator::SmokeGenerator(){};
+bool compareByLifetime(SmokeParticle p1, SmokeParticle p2){
+  return (p1.lifetime > p2.lifetime);
+}
+
+SmokeGenerator::SmokeGenerator(){
+  // Create smoke particles
+  for(int p = 0; p < maxNbParticles; p++){
+    SmokeParticle particle;
+    particle.speed = {0.0, 0.0, 0.0};
+    particle.position = {0.0, 0.0, 0.0};
+    particle.size = 0.0;
+    particle.lifetime = 0.0;
+
+    smokeParticles.push_back(particle);
+  }
+
+  // Start clock
+  innerClock = new sf::Clock();
+};
 
 void SmokeGenerator::initBuffers(){
-  static const GLfloat vertexBuffer[] = {
-    -0.5f, 0.0f, -0.5f,
-    0.5f, 0.0f, -0.5f,
-    -0.5f, 0.0f, 0.5f,
-    0.5f, 0.0f, 0.5f,
-  };
-
   // Vertex buffer
   glGenBuffers(1, &vertexBufferId);
   glBindBuffer(GL_ARRAY_BUFFER, vertexBufferId);
@@ -35,11 +50,81 @@ void SmokeGenerator::initBuffers(){
     GL_STREAM_DRAW);
 };
 
-void SmokeGenerator::generate(sf::Vector3f position){
+void SmokeGenerator::generate(sf::Vector3f position, int numberParticles){
+  if(nbParticles + numberParticles > maxNbParticles){
+    std::cout << "Cannot create more particles..." << std::endl;
 
+    return;
+  }
+
+  // Random generators
+  std::uniform_real_distribution<float> getPosition(-1.0, 1.0);
+  std::uniform_real_distribution<float> getSize(1.0, 1.5);
+  std::uniform_real_distribution<float> getLifetime(2.0, 3.0);
+
+  // Create particles
+  for(int p = nbParticles; p < nbParticles + numberParticles; p++){
+    SmokeParticle& particle = smokeParticles.at(p);
+    particle.speed = {0.1, 0.1, 0.2};
+    particle.position = {
+      position.x + getPosition(generator),
+      position.y + getPosition(generator),
+      position.z + getPosition(generator)
+    };
+    particle.size = getSize(generator);
+    particle.lifetime = getLifetime(generator);
+  }
+
+  // Sort particles by lifetime
+  std::sort(smokeParticles.begin(), smokeParticles.end(), compareByLifetime);
+
+  nbParticles += numberParticles;
 };
 
 void SmokeGenerator::draw(){
+  float timeSinceLastCall = innerClock->getElapsedTime().asSeconds();
+
+  // Update positionSizeBuffer
+  for(int p = 0; p < nbParticles; p++){
+    SmokeParticle& particle = smokeParticles.at(p);
+
+    // Update particle lifetime
+    particle.lifetime -= timeSinceLastCall;
+    if(particle.lifetime <= 0.0){
+      particle.lifetime = 0.0;
+      nbParticles -= 1;
+
+      continue;
+    }
+
+    // Update particle position
+    particle.position.x += particle.speed.x * timeSinceLastCall;
+    particle.position.y += particle.speed.y * timeSinceLastCall;
+    particle.position.z += particle.speed.z * timeSinceLastCall;
+
+    // Update particle size
+    particle.size *= particle.lifetime;
+
+    // Update the buffer
+    positionSizeBuffer[4 * p + 0] = particle.position.x;
+    positionSizeBuffer[4 * p + 1] = particle.position.y;
+    positionSizeBuffer[4 * p + 2] = particle.position.z;
+    positionSizeBuffer[4 * p + 3] = particle.size;
+  }
+
+  // Bind the new positionSizeBuffer
+  glBindBuffer(GL_ARRAY_BUFFER, positionSizeBufferId);
+  glBufferData(
+    GL_ARRAY_BUFFER,
+    maxNbParticles * 4 * sizeof(GLfloat),
+    NULL,
+    GL_STREAM_DRAW);
+  glBufferSubData(
+    GL_ARRAY_BUFFER,
+    0,
+    maxNbParticles * 4 * sizeof(GLfloat),
+    positionSizeBuffer);
+
   // Send vertices
   glEnableVertexAttribArray(0);
   glBindBuffer(GL_ARRAY_BUFFER, vertexBufferId);
@@ -50,8 +135,16 @@ void SmokeGenerator::draw(){
   glBindBuffer(GL_ARRAY_BUFFER, positionSizeBufferId);
   glVertexAttribPointer(1, 4, GL_FLOAT, GL_FALSE, 0, (void*)0);
 
+  // Always use 4 vertices
+  glVertexAttribDivisor(0, 0);
+  // Always use one position/size per quad
+  glVertexAttribDivisor(1, 1);
+
   // Draw triangles
   glDrawArraysInstanced(GL_TRIANGLE_STRIP, 0, 4, nbParticles);
+
+  // Restart clock
+  innerClock->restart();
 };
 
 SmokeGenerator::~SmokeGenerator(){
--- src/SmokeGenerator/SmokeGenerator.hxx
+++ src/SmokeGenerator/SmokeGenerator.hxx
@@ -1,23 +1,52 @@
 #ifndef SMOKEGENERATOR_HXX_
 #define SMOKEGENERATOR_HXX_
 
+#include <vector>
+
 #include <GL/gl.h>
 
 #include <SFML/Graphics.hpp>
 
+struct SmokeParticle {
+  sf::Vector3f speed;
+  sf::Vector3f position;
+  float size;
+  float lifetime;
+};
+
 class SmokeGenerator {
 private:
   /* Max number of particles */
-  const int maxNbParticles = 50;
+  static const int maxNbParticles = 500;
 
-  /* Number of particles */
+  /* Current number of particles */
   int nbParticles = 0;
 
   /* ID of the vertex buffer */
   GLuint vertexBufferId;
 
+  /* The vertex buffer (same for each smoke particle) */
+  const GLfloat vertexBuffer[12] = {
+    -0.5f, 0.0f, -0.5f,
+    0.5f, 0.0f, -0.5f,
+    -0.5f, 0.0f, 0.5f,
+    0.5f, 0.0f, 0.5f,
+  };
+
   /* ID of the position/size buffer */
   GLuint positionSizeBufferId;
+
+  /* The buffer containing the positions and sizes of smoke particles */
+  GLfloat positionSizeBuffer[maxNbParticles];
+
+  /* The array of smoke particles */
+  std::vector<SmokeParticle> smokeParticles;
+
+  /* Random generator for the lifetime, speed, position and size */
+  std::default_random_engine generator;
+
+  /* Inner clock for updating particles */
+  sf::Clock* innerClock;
 
   /* Delete buffers from memory */
   void deleteBuffers();
@@ -31,8 +60,9 @@ public:
 
   /* Generate smoke at the given position
     \param position: Position where to generate the smoke
+    \param numberParticles: Number of smoke particles to create
   */
-  void generate(sf::Vector3f position);
+  void generate(sf::Vector3f position, int numberParticles);
 
   /* Draw smoke in the currently bound framebuffer object */
   void draw();
