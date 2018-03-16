fad1dec771d1127ff5f103b673dac15b61bf7b78
martinRenou <martin.renou@gmail.com>
2018-03-16 14:12:24 +0100

    If there is no smoke particle to display, the draw method does nothing

--- src/SmokeGenerator/SmokeGenerator.cxx
+++ src/SmokeGenerator/SmokeGenerator.cxx
@@ -106,64 +106,66 @@ void SmokeGenerator::generate(sf::Vector3f position, int numberParticles){
 void SmokeGenerator::draw(){
   float timeSinceLastCall = innerClock->getElapsedTime().asSeconds();
 
-  // Update positionSizeBuffer
-  for(int p = 0; p < nbParticles; p++){
-    SmokeParticle& particle = smokeParticles.at(p);
+  if(nbParticles > 0){
+    // Update positionSizeBuffer
+    for(int p = 0; p < nbParticles; p++){
+      SmokeParticle& particle = smokeParticles.at(p);
 
-    // Update particle lifetime
-    particle.lifetime -= timeSinceLastCall;
-    if(particle.lifetime <= 0.0){
-      particle.lifetime = 0.0;
-      nbParticles -= 1;
+      // Update particle lifetime
+      particle.lifetime -= timeSinceLastCall;
+      if(particle.lifetime <= 0.0){
+        particle.lifetime = 0.0;
+        nbParticles -= 1;
 
-      continue;
+        continue;
+      }
+
+      // Update particle position
+      particle.position.x += particle.speed.x * timeSinceLastCall;
+      particle.position.y += particle.speed.y * timeSinceLastCall;
+      particle.position.z += particle.speed.z * timeSinceLastCall;
+
+      // Update particle size
+      particle.size *= particle.lifetime;
+
+      // Update the buffer
+      positionSizeBuffer[4 * p + 0] = particle.position.x;
+      positionSizeBuffer[4 * p + 1] = particle.position.y;
+      positionSizeBuffer[4 * p + 2] = particle.position.z;
+      positionSizeBuffer[4 * p + 3] = particle.size;
     }
 
-    // Update particle position
-    particle.position.x += particle.speed.x * timeSinceLastCall;
-    particle.position.y += particle.speed.y * timeSinceLastCall;
-    particle.position.z += particle.speed.z * timeSinceLastCall;
+    // Bind the new positionSizeBuffer
+    glBindBuffer(GL_ARRAY_BUFFER, positionSizeBufferId);
+    glBufferData(
+      GL_ARRAY_BUFFER,
+      maxNbParticles * 4 * sizeof(GLfloat),
+      NULL,
+      GL_STREAM_DRAW);
+    glBufferSubData(
+      GL_ARRAY_BUFFER,
+      0,
+      maxNbParticles * 4 * sizeof(GLfloat),
+      positionSizeBuffer);
 
-    // Update particle size
-    particle.size *= particle.lifetime;
+    // Send vertices
+    glEnableVertexAttribArray(0);
+    glBindBuffer(GL_ARRAY_BUFFER, vertexBufferId);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, (void*)0);
 
-    // Update the buffer
-    positionSizeBuffer[4 * p + 0] = particle.position.x;
-    positionSizeBuffer[4 * p + 1] = particle.position.y;
-    positionSizeBuffer[4 * p + 2] = particle.position.z;
-    positionSizeBuffer[4 * p + 3] = particle.size;
+    // Send positions and sizes
+    glEnableVertexAttribArray(1);
+    glBindBuffer(GL_ARRAY_BUFFER, positionSizeBufferId);
+    glVertexAttribPointer(1, 4, GL_FLOAT, GL_FALSE, 0, (void*)0);
+
+    // Always use 4 vertices
+    glVertexAttribDivisor(0, 0);
+    // Always use one position/size per quad
+    glVertexAttribDivisor(1, 1);
+
+    // Draw triangles
+    glDrawArraysInstanced(GL_TRIANGLE_STRIP, 0, 4, nbParticles);
   }
-
-  // Bind the new positionSizeBuffer
-  glBindBuffer(GL_ARRAY_BUFFER, positionSizeBufferId);
-  glBufferData(
-    GL_ARRAY_BUFFER,
-    maxNbParticles * 4 * sizeof(GLfloat),
-    NULL,
-    GL_STREAM_DRAW);
-  glBufferSubData(
-    GL_ARRAY_BUFFER,
-    0,
-    maxNbParticles * 4 * sizeof(GLfloat),
-    positionSizeBuffer);
-
-  // Send vertices
-  glEnableVertexAttribArray(0);
-  glBindBuffer(GL_ARRAY_BUFFER, vertexBufferId);
-  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, (void*)0);
-
-  // Send positions and sizes
-  glEnableVertexAttribArray(1);
-  glBindBuffer(GL_ARRAY_BUFFER, positionSizeBufferId);
-  glVertexAttribPointer(1, 4, GL_FLOAT, GL_FALSE, 0, (void*)0);
-
-  // Always use 4 vertices
-  glVertexAttribDivisor(0, 0);
-  // Always use one position/size per quad
-  glVertexAttribDivisor(1, 1);
-
-  // Draw triangles
-  glDrawArraysInstanced(GL_TRIANGLE_STRIP, 0, 4, nbParticles);
 
   // Restart clock
   innerClock->restart();
