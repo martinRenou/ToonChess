cfa8465517247ac42fd7c82a32377db5792512ba
martinRenou <martin.renou@gmail.com>
2018-03-20 11:03:34 +0100

    Smoke particles now faces the camera

--- shaders/smokeVS.glsl
+++ shaders/smokeVS.glsl
@@ -11,8 +11,12 @@ void main(void){
   vec3 particleCenter = center_size.xyz;
   float particleSize = center_size.w;
 
-  vec3 position = particleCenter + vec3(
-    vertexPosition.x * particleSize, 0.0, vertexPosition.z * particleSize);
+  vec3 camera_right = vec3(VMatrix[0][0], VMatrix[1][0], VMatrix[2][0]);
+  vec3 camera_up = vec3(VMatrix[0][1], VMatrix[1][1], VMatrix[2][1]);
+
+  vec3 position = particleCenter
+    + camera_right * vertexPosition.x * particleSize
+    + camera_up * vertexPosition.z * particleSize;
 
   // Compute UV coordinates
   UV = vertexPosition.xz + vec2(0.5, 0.5);
