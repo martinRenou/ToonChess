bbeee4cd596fd74e32896fb152964875ddc2e343
martinRenou <martin.renou@gmail.com>
2018-12-29 20:37:15 +0100

    Clean color picking

--- src/ColorPicking/ColorPicking.cxx
+++ src/ColorPicking/ColorPicking.cxx
@@ -13,178 +13,192 @@
 
 #include "ColorPicking.hxx"
 
-struct Pixel {
-  GLfloat r;
-  GLfloat g;
-  GLfloat b;
+struct Pixel
+{
+    GLfloat r;
+    GLfloat g;
+    GLfloat b;
 };
 
 /* Makes a color-picking rendering in the current framebuffer
-  \param game The game instance
-  \param meshes The map of meshes
-  \param programs The map of shader programs
-  \param camera The camera
+    \param game The game instance
+    \param meshes The map of meshes
+    \param programs The map of shader programs
+    \param camera The camera
 */
 void colorPickingRender(
-    ChessGame* game,
-    std::map<int, Mesh*>* meshes,
-    std::map<int, ShaderProgram*>* programs,
-    Camera* camera, float elapsedTime){
-  // The movement Matrix
-  std::vector<GLfloat> movementMatrix;
-  Vector3f translation;
-  Vector3f rotation = {0, 0, 1};
+        const ChessGame& game,
+        const std::map<int, Mesh>& meshes,
+        std::map<int, ShaderProgram>& programs,
+        const Camera& camera, float elapsedTime)
+{
+    Vector3f rotation(0, 0, 1);
 
-  // Get shader program
-  ShaderProgram* colorPickingProgram = programs->at(COLOR_PICKING);
+    // Get shader program
+    ShaderProgram& colorPickingProgram = programs.at(COLOR_PICKING);
 
-  // Render everything with color depending on the position
-  glUseProgram(colorPickingProgram->id);
-  glCullFace(GL_BACK);
+    // Render everything with color depending on the position
+    glUseProgram(colorPickingProgram.id);
+    glCullFace(GL_BACK);
 
-  // Bind uniform values
-  colorPickingProgram->setViewMatrix(&camera->viewMatrix);
-  colorPickingProgram->setProjectionMatrix(&camera->projectionMatrix);
+    // Bind uniform values
+    colorPickingProgram.setViewMatrix(camera.viewMatrix);
+    colorPickingProgram.setProjectionMatrix(camera.projectionMatrix);
 
-  for(int x = 0; x < 8; x++){
-    for(int y = 0; y < 8; y++){
-      int piece = game->board[x][y];
+    for(int x = 0; x < 8; x++)
+    {
+        for(int y = 0; y < 8; y++)
+        {
+            int piece = game.board[x][y];
 
-      // Set movement matrix
-      movementMatrix = getIdentityMatrix();
+            // Set movement matrix
+            auto movementMatrix = getIdentityMatrix();
 
-      // Rotate the piece depending on the team
-      movementMatrix = piece > 0 ?
-        rotate(&movementMatrix, -90.0, rotation) :
-        rotate(&movementMatrix, 90.0, rotation);
+            // Rotate the piece depending on the team
+            movementMatrix = piece > 0 ?
+                rotate(&movementMatrix, -90.0, rotation) :
+                rotate(&movementMatrix, 90.0, rotation);
 
-      // Translate the piece
-      translation = {(float)(x * 4.0 - 14.0), (float)(y * 4.0 - 14.0), 0.0};
-      // If it's part of the suggested move
-      if((game->suggestedUserMoveStartPosition.x == x and
-          game->suggestedUserMoveStartPosition.y == y) or
-          (game->suggestedUserMoveEndPosition.x == x and
-          game->suggestedUserMoveEndPosition.y == y)){
-        translation.z = 0.5 + 0.5 * sin(2*elapsedTime - M_PI/2.0);
-      }
-      movementMatrix = translate(&movementMatrix, translation);
-      colorPickingProgram->setMoveMatrix(&movementMatrix);
+            // Translate the piece
+            Vector3f translation((float)(x * 4.0 - 14.0), (float)(y * 4.0 - 14.0), 0.0);
+            // If it's part of the suggested move
+            if((game.suggestedUserMoveStartPosition.x == x and
+                    game.suggestedUserMoveStartPosition.y == y) or
+                    (game.suggestedUserMoveEndPosition.x == x and
+                    game.suggestedUserMoveEndPosition.y == y))
+            {
+                translation.z = 0.5 + 0.5 * sin(2*elapsedTime - M_PI/2.0);
+            }
+            movementMatrix = translate(movementMatrix, translation);
+            colorPickingProgram.setMoveMatrix(movementMatrix);
 
-      // Set color depending on the position
-      colorPickingProgram->setVector4f("color", x/8.0, y/8.0, 0.0, 1.0);
+            // Set color depending on the position
+            colorPickingProgram.setVector4f("color", x/8.0, y/8.0, 0.0, 1.0);
 
-      // Display board cell
-      meshes->at(BOARDCELL)->draw();
+            // Display board cell
+            meshes.at(BOARDCELL).draw();
 
-      if(piece != EMPTY) meshes->at(abs(piece))->draw();
+            if (piece != EMPTY)
+            {
+                meshes.at(abs(piece)).draw();
+            }
+        }
     }
-  }
 };
 
-ColorPicking::ColorPicking(GLuint width, GLuint height) :
-  width{width}, height{height}{}
-
-void ColorPicking::initBuffers(){
-  // Create frameBuffer object
-  glGenFramebuffers(1, &fboId);
-  glBindFramebuffer(GL_FRAMEBUFFER, fboId);
-
-  // Create render buffer for color
-  glGenRenderbuffers(1, &colorRenderBufferId);
-  glBindRenderbuffer(GL_RENDERBUFFER, colorRenderBufferId);
-  glRenderbufferStorage(GL_RENDERBUFFER, GL_RGBA, width, height);
-  glFramebufferRenderbuffer(
-    GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0,
-    GL_RENDERBUFFER, colorRenderBufferId
-  );
-
-  // Create render buffer for depth test
-  glGenRenderbuffers(1, &depthRenderBufferId);
-  glBindRenderbuffer(GL_RENDERBUFFER, depthRenderBufferId);
-  glRenderbufferStorage(GL_RENDERBUFFER, GL_DEPTH_COMPONENT, width, height);
-  glFramebufferRenderbuffer(
-    GL_FRAMEBUFFER, GL_DEPTH_ATTACHMENT,
-    GL_RENDERBUFFER, depthRenderBufferId
-  );
-
-  if(glCheckFramebufferStatus(GL_FRAMEBUFFER) != GL_FRAMEBUFFER_COMPLETE){
-    std::cout << "Error while creating the FBO for color picking" << std::endl;
-  }
-
-  // Unbind buffers
-  glBindRenderbuffer(GL_RENDERBUFFER, 0);
-  glBindFramebuffer(GL_FRAMEBUFFER, 0);
+ColorPicking::ColorPicking(GLuint width, GLuint height)
+    : width(width), height(height)
+{
 }
 
-void ColorPicking::resizeBuffers(GLuint newWidth, GLuint newHeight){
-  // If the resize is a shrinking of the screen, then don't do anything because
-  // the buffer will do the job
-  if(width >= newWidth and height >= newHeight){
+void ColorPicking::initBuffers()
+{
+    // Create frameBuffer object
+    glGenFramebuffers(1, &fboId);
+    glBindFramebuffer(GL_FRAMEBUFFER, fboId);
+
+    // Create render buffer for color
+    glGenRenderbuffers(1, &colorRenderBufferId);
+    glBindRenderbuffer(GL_RENDERBUFFER, colorRenderBufferId);
+    glRenderbufferStorage(GL_RENDERBUFFER, GL_RGBA, width, height);
+    glFramebufferRenderbuffer(
+        GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0,
+        GL_RENDERBUFFER, colorRenderBufferId
+    );
+
+    // Create render buffer for depth test
+    glGenRenderbuffers(1, &depthRenderBufferId);
+    glBindRenderbuffer(GL_RENDERBUFFER, depthRenderBufferId);
+    glRenderbufferStorage(GL_RENDERBUFFER, GL_DEPTH_COMPONENT, width, height);
+    glFramebufferRenderbuffer(
+        GL_FRAMEBUFFER, GL_DEPTH_ATTACHMENT,
+        GL_RENDERBUFFER, depthRenderBufferId
+    );
+
+    if(glCheckFramebufferStatus(GL_FRAMEBUFFER) != GL_FRAMEBUFFER_COMPLETE)
+    {
+        std::cout << "Error while creating the FBO for color picking" << std::endl;
+    }
+
+    // Unbind buffers
+    glBindRenderbuffer(GL_RENDERBUFFER, 0);
+    glBindFramebuffer(GL_FRAMEBUFFER, 0);
+}
+
+void ColorPicking::resizeBuffers(GLuint newWidth, GLuint newHeight)
+{
+    // If the resize is a shrinking of the screen, then don't do anything because
+    // the buffer will do the job
+    if(width >= newWidth and height >= newHeight)
+    {
+        width = newWidth;
+        height = newHeight;
+
+        return;
+    };
+
+    // Change the size for the buffers
     width = newWidth;
     height = newHeight;
 
-    return;
-  };
+    deleteBuffers();
 
-  // Change the size for the buffers
-  width = newWidth;
-  height = newHeight;
-
-  deleteBuffers();
-
-  // Recreate buffers with the new size
-  initBuffers();
+    // Recreate buffers with the new size
+    initBuffers();
 }
 
-void ColorPicking::deleteBuffers(){
-  // Delete framebuffer
-  GLuint framebuffer[1] = {fboId};
-  glDeleteFramebuffers(1, framebuffer);
+void ColorPicking::deleteBuffers()
+{
+    // Delete framebuffer
+    GLuint framebuffer[1] = {fboId};
+    glDeleteFramebuffers(1, framebuffer);
 
-  // Delete render buffers
-  GLuint buffers[2] = {colorRenderBufferId, depthRenderBufferId};
-  glDeleteRenderbuffers(2, buffers);
+    // Delete render buffers
+    GLuint buffers[2] = {colorRenderBufferId, depthRenderBufferId};
+    glDeleteRenderbuffers(2, buffers);
 }
 
 Vector2i ColorPicking::getClickedPiecePosition(
-    Vector2i clickedPixelPosition,
-    ChessGame* game,
-    std::map<int, Mesh*>* meshes,
-    std::map<int, ShaderProgram*>* programs,
-    Camera* camera, float elapsedTime){
-  // Bind the framebuffer
-  glBindFramebuffer(GL_FRAMEBUFFER, fboId);
+        const Vector2i& clickedPixelPosition,
+        const ChessGame& game,
+        const std::map<int, Mesh>& meshes,
+        const std::map<int, ShaderProgram>& programs,
+        const Camera& camera, float elapsedTime)
+{
+    // Bind the framebuffer
+    glBindFramebuffer(GL_FRAMEBUFFER, fboId);
 
-  // Clear buffers and render
-  glClearColor(1, 1, 1, 1);
-  glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    // Clear buffers and render
+    glClearColor(1, 1, 1, 1);
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-  glViewport(0, 0, width, height);
+    glViewport(0, 0, width, height);
 
-  colorPickingRender(game, meshes, programs, camera, elapsedTime);
+    colorPickingRender(game, meshes, programs, camera, elapsedTime);
 
-  // Get pixel color at clicked position
-  Pixel pixel;
-  glReadPixels(
-    clickedPixelPosition.x, clickedPixelPosition.y,
-    1, 1,
-    GL_RGB, GL_FLOAT,
-    &pixel
-  );
+    // Get pixel color at clicked position
+    Pixel pixel;
+    glReadPixels(
+        clickedPixelPosition.x, clickedPixelPosition.y,
+        1, 1,
+        GL_RGB, GL_FLOAT,
+        &pixel
+    );
 
-  // Get piece position according to picked color
-  Vector2i clickedPiecePosition;
-  int selectedX = (int)round(pixel.r*8);
-  int selectedY = (int)round(pixel.g*8);
-  clickedPiecePosition.x = (selectedX == 8) ? -1 : selectedX;
-  clickedPiecePosition.y = (selectedY == 8) ? -1 : selectedY;
+    // Get piece position according to picked color
+    Vector2i clickedPiecePosition;
+    int selectedX = (int)round(pixel.r * 8);
+    int selectedY = (int)round(pixel.g * 8);
+    clickedPiecePosition.x = (selectedX == 8) ? -1 : selectedX;
+    clickedPiecePosition.y = (selectedY == 8) ? -1 : selectedY;
 
-  // Unbind framebuffer
-  glBindFramebuffer(GL_FRAMEBUFFER, 0);
+    // Unbind framebuffer
+    glBindFramebuffer(GL_FRAMEBUFFER, 0);
 
-  return clickedPiecePosition;
+    return clickedPiecePosition;
 }
 
-ColorPicking::~ColorPicking(){
-  deleteBuffers();
+ColorPicking::~ColorPicking()
+{
+    deleteBuffers();
 }
--- src/ColorPicking/ColorPicking.hxx
+++ src/ColorPicking/ColorPicking.hxx
@@ -12,60 +12,61 @@
 #include "../shader/ShaderProgram.hxx"
 #include "../ChessGame/ChessGame.hxx"
 
-class ColorPicking {
-private:
-  /* The identifier of the framebuffer object */
-  GLuint fboId;
-
-  /* The identifier of the renderbuffer for color */
-  GLuint colorRenderBufferId;
-
-  /* The identifier of the renderbuffer for depth */
-  GLuint depthRenderBufferId;
-
-  /* The width of the renderbuffer */
-  GLuint width;
-
-  /* The height of the renderbuffer */
-  GLuint height;
-
-  /* Delete buffers from memory */
-  void deleteBuffers();
-
+class ColorPicking
+{
 public:
-  /* Constructor
-    \param width The screen width
-    \param height The screen height
-  */
-  ColorPicking(GLuint width, GLuint height);
 
-  /* Initialization of the buffer objects */
-  void initBuffers();
+    /* Constructor
+        \param width The screen width
+        \param height The screen height
+    */
+    ColorPicking(GLuint width, GLuint height);
 
-  /* Function that must be called when resizing the screen
-    \param width The screen width
-    \param height The screen height
-  */
-  void resizeBuffers(GLuint newWidth, GLuint newHeight);
+    /* Initialization of the buffer objects */
+    void initBuffers();
 
-  /* Get the cliked piece position according to the clicked pixel on the screen.
-    This will perform a rendering in the framebuffer
-    \param clickedPixelPosition The position of the clicked pixel on the screen
-    \param game The game instance
-    \param meshes The map of piece meshes
-    \param programs The map of shader programs
-    \param camera The camera
-    \return The position of the clicked chess piece
-  */
-  Vector2i getClickedPiecePosition(
-    Vector2i clickedPixelPosition,
-    ChessGame* game,
-    std::map<int, Mesh*>* meshes,
-    std::map<int, ShaderProgram*>* programs,
-    Camera* camera, float elapsedTime);
+    /* Function that must be called when resizing the screen
+        \param width The screen width
+        \param height The screen height
+    */
+    void resizeBuffers(GLuint newWidth, GLuint newHeight);
 
-  /* Destructor, this will remove the buffers from memory */
-  ~ColorPicking();
+    /* Get the cliked piece position according to the clicked pixel on the screen.
+        This will perform a rendering in the framebuffer
+        \param clickedPixelPosition The position of the clicked pixel on the screen
+        \param game The game instance
+        \param meshes The map of piece meshes
+        \param programs The map of shader programs
+        \param camera The camera
+        \return The position of the clicked chess piece
+    */
+    Vector2i getClickedPiecePosition(
+        const Vector2i& clickedPixelPosition,
+        const ChessGame& game,
+        const std::map<int, Mesh>& meshes,
+        const std::map<int, ShaderProgram>& programs,
+        const Camera& camera, float elapsedTime);
+
+    /* Destructor, this will remove the buffers from memory */
+    ~ColorPicking();
+
+private:
+
+    /* The identifier of the framebuffer object */
+    GLuint fboId;
+
+    /* The identifier of the renderbuffer for color */
+    GLuint colorRenderBufferId;
+
+    /* The identifier of the renderbuffer for depth */
+    GLuint depthRenderBufferId;
+
+    /* Size of the renderbuffer */
+    GLuint width;
+    GLuint height;
+
+    /* Delete buffers from memory */
+    void deleteBuffers();
 };
 
 #endif
